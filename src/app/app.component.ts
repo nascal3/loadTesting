93d0fef923aa2c9ddbd08b0844c821587124b6f3
nascal3 <nascal3@gmail.com>
2018-02-11 00:10:03 +0300

    load more tiles test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  dataLength: number;
+  showAmount = 18;
+  seeMore: boolean;
+  url: string;
+  people: any;
+
+  constructor( private http: HttpClient) {
+    this.getStuff();
+    this.seeMoreVisibiltiy();
+  }
+
+  seeMoreVisibiltiy() {
+    this.dataLength >= this.showAmount ? this.seeMore = true : this.seeMore = false;
+  }
+
+  getStuff() {
+    this.url = 'https://jsonplaceholder.typicode.com/posts';
+    this.http.get(this.url).subscribe( res => {
+      this.people = res;
+      this.dataLength = this.people.length;
+      console.log(res);
+    });
+  }
+
+  loadMore() {
+    if (this.dataLength > this.showAmount ) {
+      this.showAmount = this.showAmount + 18;
+      console.log(this.showAmount);
+    }
+
+    this.dataLength <= this.showAmount ? this.seeMore = true : this.seeMore = false;
+
+  }
+
+
 }
